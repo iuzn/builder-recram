fcde4ee55a877b5fedb344317742075126454a0d
ibrahim <info@ibrahimuzun.com>
2023-10-16 16:37:47 +0300

    Remove unnecessary div wrapper in Sidebar component

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -33,18 +33,17 @@ function getIconComponent(type: keyof typeof icons) {
       :selectedElement="store.selectedElement"
       :key="store.selectedElement.id"
     />
-    <div v-else class="w-full">
-      <button
-        id="sidebar-button"
-        v-for="element in store.sidebarElements"
-        :key="element.type"
-        @dragstart="dragStart(element, $event)"
-        draggable="true"
-        class="flex px-3 py-2 gap-3.5 items-center justify-start hover:text-primary cursor-grab active:cursor-grabbing group hover:shadow-base rounded"
-      >
-        <component :is="getIconComponent(element.icon)" class="group-hover:fill-primary" />
-        {{ element.type }}
-      </button>
-    </div>
+    <button
+      v-else
+      id="sidebar-button"
+      v-for="element in store.sidebarElements"
+      :key="element.type"
+      @dragstart="dragStart(element, $event)"
+      draggable="true"
+      class="flex px-3 py-2 gap-3.5 items-center justify-start hover:text-primary cursor-grab active:cursor-grabbing group hover:shadow-base rounded"
+    >
+      <component :is="getIconComponent(element.icon)" class="group-hover:fill-primary" />
+      {{ element.type }}
+    </button>
   </div>
 </template>
